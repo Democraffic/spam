435622eacc87ee5b567854a4f27c602576ec9784
Maulikk Chevli <maulikk@pop-os.localdomain>
2021-11-27 19:46:09 +0100

    Add description spam route

--- app.py
+++ app.py
@@ -15,3 +15,17 @@ API_ROUTE = {
 @app.route("/")
 def index():
     return "Hi! This is the index page of spam utils."
+
+
+@app.route("/spam/check_description", methods = ['POST', 'GET'])
+def check_spam_on_description():
+
+    id = request.json["id"]
+    description = request.json["description"]
+
+    response = {
+        "id": id,
+        "isSpam": description
+    }
+
+    return jsonify(response)
